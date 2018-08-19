d8f872f8587d5be17152217a6e96e172401ce3ef
zhipengli0524 <lizhipeng524@gmail.com>
2018-08-19 22:46:41 +0800

    add new interface cut_by_opencv

--- ICL/img_cut.py
+++ ICL/img_cut.py
@@ -6,13 +6,23 @@ from PIL import Image
 import sys
 import logging
 import os
+import numpy
+import cv2
+
 
 class ImageCutter(object):
     """cut images
     Attributes:
     """
-    def __init__(self):
+    def __init__(self, cv_model_file=None):
         super(ImageCutter, self).__init__()
+        if cv_model_file is not None:
+            self._cv_model_file = cv_model_file
+        else:
+            file_dir = os.path.dirname(__file__)
+            file_dir = os.path.abspath(file_dir)
+            self._cv_model_file = os.path.join(file_dir, "cv_model", "frontalface.xml")
+        self._face_cascade = cv2.CascadeClassifier(self._cv_model_file)
 
     def cut_by_half_center(self, image, resize=None):
         """Cut the Image by hale center
@@ -30,6 +40,37 @@ class ImageCutter(object):
         box = (start_point_xy, start_point_xy, 
                end_point_xy, end_point_xy)
         new_im = im.crop(box)
+        if resize is not None:
+            new_im = self.resize(new_im, resize)
+        return new_im
+
+    def cut_by_opencv(self, image, resize=None):
+        """Cut the image by open cv
+        Args:
+            image str (path of image file) or Image object
+        Returns:
+            PIL.Image.Image
+        Raises:
+            TypeError
+        """
+        im = self._openImage(image)
+        imcv = cv2.cvtColor(numpy.asarray(im), cv2.COLOR_RGB2BGR)
+        gray = cv2.cvtColor(imcv, cv2.COLOR_BGR2GRAY)
+        faces = self._face_cascade.detectMultiScale(
+                   gray,
+                   scaleFactor = 1.2, 
+                   minNeighbors = 5,
+                   minSize = (5,5),
+                   flags = 0
+                )
+        if len(faces) <= 0:
+            return None
+
+        face = faces[self._bigIndex(faces)]
+        x, y, w, h = face
+        new_imcv = image[y:y+h, x:x+w]
+        new_im = Image.fromarray(cv2.cvtColor(new_imcv, cv2.COLOR_BGR2RGB))
+
         if resize is not None:
             new_im = self.resize(new_im, resize)
         return new_im
@@ -70,6 +111,29 @@ class ImageCutter(object):
             raise TypeError("Input Type Error: only str (path of image file) \
                              and PIL.Image.Image object acceptable.")
         return im
+
+    def _bigIndex(self, faces):
+        """To get index of biggest face
+        Args:
+            faces   list [(x, y, w, h), (...), ...]
+        Returns:
+            int     index
+        Raises:
+            None
+        """
+        if len(faces) <= 0:
+            return -1
+        
+        bigIndex = 0
+        aera = 0
+        for idx in range(len(faces)):
+            x, y, w, h = faces[idx]
+            thisAera = w * h
+            if thisAera > aera:
+                aera = thisAera
+                bigIndex = idx
+
+        return bigIndex
 
 
 
